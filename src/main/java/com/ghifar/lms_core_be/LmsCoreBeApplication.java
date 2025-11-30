65d206774b66332c4a47f889cf5b450960ae89cc
Fathullah Ghifar <fghifarr.dev@gmail.com>
2025-11-30 09:34:45 +0700

    first commit: spring initializr

---
+++ src/main/java/com/ghifar/lms_core_be/LmsCoreBeApplication.java
@@ -0,0 +1,13 @@
+package com.ghifar.lms_core_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LmsCoreBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LmsCoreBeApplication.class, args);
+	}
+
+}
